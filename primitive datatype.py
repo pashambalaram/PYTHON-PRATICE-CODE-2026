ffb6dd8523c068ad1da91b14ab51f41a851264e2
pashambalaram <balarampasham3@gmail.com>
2026-04-16 18:59:56 +0530

    Add files via upload

---
+++ primitive datatype.py
@@ -0,0 +1,67 @@
+# Integer Data types
+# a=10
+# print(a)
+# print(type(a))
+# print(id(a))
+
+
+#Binary Code           base value=2
+# a=0b1011             limit=>0-1
+# print(a)              
+# print(type(a))        
+# print(id(a))
+
+
+#Oct code              base value=>8
+# a=0o1234567          limit=>0-7
+# print(a)
+# print(type(a))
+# print(id(a))
+
+
+# Hex code             base value=>16
+#a=0x1234a567890       limit=>0-9,A-F
+# print(a)
+# print(type(a))
+# print(id(a))
+
+# Float datatype
+# a=10.5
+# print(a)
+# print(type(a))
+# print(id(a))
+
+# b=10/5
+# print(b)
+# print(type(b))
+
+# c=4e3
+# print(c)
+# print(type(c))
+
+
+# String datatype
+# a="hello"
+# b="balaram"
+# print(a)
+# print(type(a))
+# print(len(a))
+# print(a*3)
+# print(a +b)
+
+
+# Boolean datatype
+a=True+True
+# b=False+False        True value=>1
+# print(int(a))        False value=>0
+# print(int(b))
+# print(type(a))
+# print(type(b))
+
+
+# Complex datatype
+# a=2+3j
+# print(a)
+# print(type(a))
+# print(a.real)
+# print(a.imag)
