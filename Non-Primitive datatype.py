fab1ef0ce541e482c381b2e0fd62a99367ea645c
pashambalaram <balarampasham3@gmail.com>
2026-04-16 19:00:14 +0530

    Add files via upload

---
+++ Non-Primitive datatype.py
@@ -0,0 +1,65 @@
+# Non-primitive data types are 4 types
+# 1)list
+# 2)tuple
+# 3)set
+# 4)Dict
+
+# 1)List
+student_details=["Balaram","Gurava","Harish","Suresh",10,20.4,True]
+# print(student_details)
+
+#     Add element to the list
+# student_details.append("Ramesh")
+# print(student_details)
+
+#     Remove element from list 
+# student_details.remove("Suresh")
+# print(student_details)
+
+#     Iteration
+# for x in student_details:
+#     print(x)
+
+#    length of the list
+#  print(len(student_details))
+
+
+# 2)Tuple
+# student_details=("Balaram","Gurava","Harish","Suresh",10,20.4)
+# print(student_details)
+
+#   Iteration of the tuple
+# for x in student_details:
+#     print(x)
+
+#     length of the tuple
+# print(len(student_details))
+
+
+# 3)set     ==>set is a collection of Unique values
+# employes={"balaram","gurava","harish",10,20,10,20,(1,2,3)}
+# print(employes)
+# employes.add("Suresh")
+# print(employes)
+# employes.update({"reddy","kumar",40,50})
+# print(employes)
+# employes.remove("balaram")
+# print(employes)
+
+# 4)Dict
+# student_details={"name":"Balaram","age":20,"course":"Python"}
+# print(student_details['name'])
+
+#     Add element to the dict
+# student_details['age1']=28
+# print(student_details)
+
+#   Remove element from dict
+# student_details.pop('course')
+# print(student_details)
+
+#   Iteration of the dict
+# for x, y in student_details.items():
+#     print(x,'==', y)
+
+
