a7962c46b819c609a5184187654f53775d3de39c
pashambalaram <balarampasham3@gmail.com>
2026-04-16 17:32:12 +0530

    Add files via upload

---
+++ data types.py
@@ -0,0 +1,66 @@
+# a=10
+# print(a)
+# print(type(a))
+# print(id(a))
+#Binary Code
+# a=0b1011
+# print(a)
+# print(type(a))
+# print(id(a))
+#octa code      base value=>8
+                # limit=>0-7
+# a=0o1234567
+# print(a)
+# print(type(a))
+# print(id(a))
+# Hexa code       base value=>16
+                # limit=>0-9,A-F
+# a=0x1234a567890
+# print(a)
+# print(type(a))
+# print(id(a))
+
+# sring datatype
+a="Hello World"
+# b="World"
+# print(a)
+# print(type(a))
+# print(id(a))
+# a="1234556"
+# print(a)
+# print(type(a))
+# print(id(a))
+# print(len(a))
+# print(a*3)
+# print(a[1:5])
+# for x in a:
+#     print(x)
+# print(a.upper())
+
+
+#  boolean datatype
+#  True
+#  False
+# res=True+True
+# res1=False+False
+# print(res)
+# print(res1)
+# print(int(True))
+# print(int(False))
+
+# Float Datatype
+# a=10.4
+# print(a)
+# b=5/6
+# print(b)
+# print(type(a))
+# c=3e1
+# print(c)
+# print(type(c))
+
+# complex datatype
+# a=3+4j
+# print(a)
+# print(a.real)
+# print(type(a))
+# print(a.imag)
